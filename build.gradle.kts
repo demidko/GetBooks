7ba0cde6c8ae677916db4106396735ac23ae0ede
Daniel Demidko <demidko@farpost.com>
2021-01-17 04:49:10 +1000

    formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,38 +3,38 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.21"
-    id("org.jetbrains.compose") version "0.3.0-build140"
+  kotlin("jvm") version "1.4.21"
+  id("org.jetbrains.compose") version "0.3.0-build140"
 }
 
 group = "cf.demidko"
 version = "1.0"
 
 repositories {
-    jcenter()
-    mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+  jcenter()
+  mavenCentral()
+  maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
 }
 
 dependencies {
-    testImplementation(kotlin("test-testng"))
-    implementation(compose.desktop.currentOs)
+  testImplementation(kotlin("test-testng"))
+  implementation(compose.desktop.currentOs)
 }
 
 tasks.test {
-    useTestNG()
+  useTestNG()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+  kotlinOptions.jvmTarget = "11"
 }
 
 compose.desktop {
-    application {
-        mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "GetBooks"
-        }
+  application {
+    mainClass = "MainKt"
+    nativeDistributions {
+      targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+      packageName = "GetBooks"
     }
+  }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -9,13 +9,13 @@ import androidx.compose.runtime.setValue
 
 fun main() = Window {
 
-    var text by remember { mutableStateOf("Hello, World!") }
+  var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
+  MaterialTheme {
+    Button(onClick = {
+      text = "Hello, Desktop!"
+    }) {
+      Text(text)
     }
+  }
 }
