3f3d984a9d92ced72ffc6daf06bc29419250da6a
Daniel Demidko <demidko@farpost.com>
2021-01-17 04:50:57 +1000

    Window title

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 
-fun main() = Window {
+fun main() = Window("GetBooks") {
 
   var text by remember { mutableStateOf("Hello, World!") }
 
